a5ae60c9dfca77b531a80c4cf83312e585bc1b15
gowsalyasubramani <gowsalya2982002@gmail.com>
2024-11-28 10:01:42 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,71 @@
+import project1 from "../assets/projects/project1.webp";
+import project2 from "../assets/projects/project2.webp";
+import project3 from "../assets/projects/project3.webp";
+import project4 from "../assets/projects/project4.webp";
+
+export const HERO_CONTENT = `Hello! I’m Gowsalya, an enthusiastic and detail-oriented IT professional with a strong foundation in software development and a passion for crafting innovative solutions. I specialize in React JS and Java, with hands-on experience developing dynamic, user-friendly web applications.  My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EDUCATIONS = [
+  {
+    year: "2020 - 2024",
+    degree: "Bachelor of Technology in Information Technology",
+    institute: `University College of Engineering Nagercoil`,
+    technologies: ["CGPA - 8.54"],
+  },
+  {
+    year: "2019 - 2020",
+    degree: "HSC",
+    institute: `DMNS Dr.Sivanthi Adithanar Matriculation Higher Secondary School`,
+    technologies: ["Percentage - 80.1%"],
+  },
+  {
+    year: "2018 - 2019",
+    degree: "SSLC",
+    institute: `DMNS Dr.Sivanthi Adithanar Matriculation Higher Secondary School`,
+    technologies: ["Percentage - 96.4%"],
+  },
+  
+];
+
+export const PROJECTS = [
+  {
+    title: "Social Media Platform Development",
+    image: project1,
+    link: "https://insta-clone-1zbcpz8j8-gowsalyas-projects.vercel.app/",
+    description:
+      "Developed a social media platform enabling user signup/login, following, post interactions.",
+    technologies: ["HTML", "CSS", "JS", "React","Firebase"],
+  },
+  {
+    title: "Recipe Finder",
+    image: project2,
+    link: "https://recipe-finder-rust.vercel.app/",
+    description:
+      "Developed a recipe finder that allows user to search food items and view results.",
+    technologies: ["HTML", "CSS", "JS", "React", "Tailwind CSS"],
+  },
+  {
+    title: "TicTacToe",
+    image: project3,
+    link: "https://tictactoe-one-navy.vercel.app/",
+    description:
+      "Developed a classic Tic Tac Toe game with an intuitive interface and seamless user experience.",
+    technologies: ["HTML", "CSS", "JS", "React", "Bootstrap"],
+  },
+  {
+    title: "Code Editor",
+    image: project4,
+    link: "https://code-editor-seven-livid.vercel.app/",
+    description:
+      "Created a code editor for HTML, CSS, and JavaScript with real-time preview and interactive coding experience.",
+    technologies: ["HTML", "CSS", "JS", "React"],
+  },
+];
+
+export const CONTACT = {
+  address: "243, V.E. Road, Damodhar Nagar, Thoothukudi-628002 ",
+  phoneNo: "+91 6383096885 ",
+  email: "gowsalya2982002@gmail.com",
+};
